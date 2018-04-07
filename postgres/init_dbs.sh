377d99fbc164943bed8d527c423cbe1613ec8ae0
Nikolaos Doulgeridis <nickdoulgeridis@gmail.com>
2018-04-07 14:34:27 +0000

    [CRITICAL] Fixed env and postgresql

--- postgres/init_dbs.sh
+++ postgres/init_dbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE $OFBIZ_DB;
     CREATE DATABASE $OFBIZ_OLAP_DB;
     CREATE DATABASE $OFBIZ_TENANT_DB;
